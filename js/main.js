56be4ca73ccee82f3f365493e7abf8d3e2678df8
next537 <45046731+next537@users.noreply.github.com>
2022-02-23 19:45:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+'use strict';
+
+class ProductList {
+    constructor(container = '.products') {
+        this._container = document.querySelector(container);
+        this._goods = [];
+        this._allProducts = [];
+
+        this._fetchGoods();
+        this._render();
+    }
+
+    _fetchGoods() {
+        this._goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ];
+    }
+
+    _render() {
+        for (const product of this._goods) {
+            const productObject = new ProductItem(product);
+
+            this._allProducts.push(productObject);
+            this._container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.img = img;
+    }
+
+    getHTMLString() {
+        return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+          </div>`;
+    }
+}
+
+new ProductList();
+
+// const products = [
+//   {id: 1, title: 'Notebook', price: 20000},
+//   {id: 2, title: 'Mouse', price: 1500},
+//   {id: 3, title: 'Keyboard', price: 5000},
+//   {id: 4, title: 'Gamepad', price: 4500},
+// ];
+//
+// const renderProduct = (item, img='https://via.placeholder.com/200x150') => `<div class="product-item" data-id="${this.id}">
+//               <img src="${img}" alt="Some img">
+//               <div class="desc">
+//                   <h3>${item.title}</h3>
+//                   <p>${item.price} \u20bd</p>
+//                   <button class="buy-btn">Купить</button>
+//               </div>
+//           </div>`;
+//
+// const renderProducts = list => {
+//   document.querySelector('.products')
+//       .insertAdjacentHTML(
+//           'beforeend',
+//           list.map(item => renderProduct(item)).join('')
+//       );
+// };
+//
+// renderProducts(products);
+//
